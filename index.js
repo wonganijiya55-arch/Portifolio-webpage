1c50dbe640512833ba42a71d6aa488e1c1e9c020
wonganijiya55-arch <wonganijiya55@gmail.com>
2025-10-24 00:45:01 +0200

    version 1 of portifolio
    Portifolio without personal image

---
+++ index.js
@@ -0,0 +1,12 @@
+// Simple form alert – replace with email service later
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thanks for reaching out, " + document.getElementById("name").value + "! I’ll get back to you soon.");
+  this.reset(); // clears the form
+});
+  const menu = document.getElementById('mobile-menu');
+  const navLinks = document.querySelector('.nav-links');
+
+  menu.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+  });
